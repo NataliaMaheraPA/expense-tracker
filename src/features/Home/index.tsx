19b24b51822ae68d534864ae54cfa2996e7047bb
Natalia Mahera <nataliia.mahera@pettersonapps.com>
2025-09-02 13:29:03 +0200

    chore: initial project setup

---
+++ src/features/Home/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className="mx-auto">
+      <h1 className="text-center text-4xl font-bold">
+        Let&apos;s build a web app!
+      </h1>
+    </section>
+  );
+}
